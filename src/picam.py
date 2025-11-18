913ae355be343e6f322ab6b1b0a1c8fcf8a3c227
PorYu <yu07.24@ichat.sp.edu.sg>
2025-11-18 22:10:55 +0800

    Modified pi cam to enable scanning barcode once

--- src/picam.py
+++ src/picam.py
@@ -17,14 +17,34 @@ def runcam():
     picam2.start()
 
     sleep(1)
+
+    # Save a selfie LMAO
+    singleframe(picam2) #Moved to a seperate function
+
+    while True:
+        frame = picam2.capture_array()
+        #https://docs.opencv.org/4.x/d8/d01/group__imgproc__color__conversions.html
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        barcodes = decode(gray)
+        # if a barcode has been detetced
+        if len(barcodes) > 0:
+            data = barcodes[0].data.decode("utf-8")
+            print("Detected:", data)
+            barcode_queue.put(data)  # send barcode into queue
+            sleep(2)  # small delay to avoid double reads
+        sleep(0.1)
+
+  
+
+# SELFIE MODE
+def singleframe(picam2):
     frame = picam2.capture_array()
 
     # Save a selfie LMAO
     cv2.imwrite("single_frame.jpg", frame)
-    picam2.stop()
+
     print("Saved single_frame.jpg")
 
-  
 
 
     
