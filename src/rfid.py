fe3c2d7b571fe85da30f49a34a680a37dbc48f5e
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-20 15:16:23 +0800

    Added Basic writing using rfid

--- src/rfid.py
+++ src/rfid.py
@@ -3,11 +3,17 @@ from hal import hal_rfid_reader as rfid
 def main():
     print("testing RFID")
     reader=rfid.init()
-    temp=reader.read_id()
-    print(str(temp))
+    reader.write("12345")
+    temp=reader.read()
+    #from hal file temp is a tuple
+    #https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string
+    #get the second part of the tuple which contains the data, remove empty space and reassign
+    temp=temp[1].strip()
+    #data is already in string
+    print(temp)
 
 
 if __name__ == "__main__":
     main()
 
-# initial rfid read testing
+# Added Basic writing using rfid
