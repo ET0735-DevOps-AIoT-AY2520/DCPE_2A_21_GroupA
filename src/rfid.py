73fc1a0217d02f2ca61b5f5e4845edc773a224b1
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-20 15:17:47 +0800

    bugfix, removed unneccasry read code in setup and made reader in readmoney access global

--- src/rfid.py
+++ src/rfid.py
@@ -4,20 +4,24 @@ def setup():
     print("testing RFID")
     global reader
     reader=rfid.init()
-    reader.write("12345")
-    temp=reader.read()
     
-    temp=temp[1].strip()
+    reader.write("12345")
     #data is already in string
-    print(temp)
+    
 
 def readmoney():
+    global reader
     #from hal file temp is a tuple
     #https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string
     #get the second part of the tuple which contains the data, remove empty space and reassign
     data=reader.read()
     balance=data[1].strip()
+    print(balance)
     return balance
 
+def setmoney():
+    print("")
 
-# Moved read to seperate func, removed main and replaced with setup func
+
+
+# bugfix, removed unneccasry read code in setup and made reader in readmoney access global
