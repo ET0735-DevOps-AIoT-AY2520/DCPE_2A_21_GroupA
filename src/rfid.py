a8a0867700a5300a689051565015caba00a55c44
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-20 15:17:05 +0800

    Moved read to seperate func, removed main and replaced with setup func

--- src/rfid.py
+++ src/rfid.py
@@ -1,19 +1,23 @@
 from hal import hal_rfid_reader as rfid
 
-def main():
+def setup():
     print("testing RFID")
+    global reader
     reader=rfid.init()
     reader.write("12345")
     temp=reader.read()
-    #from hal file temp is a tuple
-    #https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string
-    #get the second part of the tuple which contains the data, remove empty space and reassign
+    
     temp=temp[1].strip()
     #data is already in string
     print(temp)
 
+def readmoney():
+    #from hal file temp is a tuple
+    #https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string
+    #get the second part of the tuple which contains the data, remove empty space and reassign
+    data=reader.read()
+    balance=data[1].strip()
+    return balance
 
-if __name__ == "__main__":
-    main()
 
-# Added Basic writing using rfid
+# Moved read to seperate func, removed main and replaced with setup func
