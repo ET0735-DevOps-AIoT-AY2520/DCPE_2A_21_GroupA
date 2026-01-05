9a6cc1f81e551edd617a9235e2d6de2bce2d3bc2
Thiha <thhtut.24@ichat.sp.edu.sg>
2026-01-05 13:46:18 +0800

    Added calling of db for data

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -1,10 +1,29 @@
+
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import Flask
 from flask import render_template
+import db
 app=Flask(__name__)
+
+def getdbdata():
+    global books
+    global profiles
+    db.getallbooks()
+    db.getallprofile()
+    books=[]
+    profiles=[]
+    for book in db.books:
+        books.append(book.to_dict())
+    for profile in db.profiles:
+        profiles.append(profile.to_dict())
+    
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    getdbdata()
+    return render_template("index.html",books=books,profiles=profiles)
 
 @app.route('/booksearch')
 def booksearch():
@@ -23,4 +42,4 @@ def logs():
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0") #0.0.0.0 accessible from all IP
 
-#Added template for logs
+#Added calling of db for data
