f8bdbf7cf0f60cf1dff0327f04474e03491b52f7
Thiha <thhtut.24@ichat.sp.edu.sg>
2026-01-05 14:19:45 +0800

    Added basic page for user fine editing

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -58,6 +58,18 @@ def bookedit():
         }
     return render_template("bookedit.html",books=jsonbook)
 
+#This function is for
+@app.route('/usersearch/edit',methods=["POST"])
+def useredit():
+    if request.method == "POST":
+        id = request.form.get("id")
+        fine = request.form.get("fine") 
+        jsonuser={
+            "id":id,
+            "fine":fine,
+        }
+    return render_template("useredit.html",users=jsonuser)
+
 @app.route('/booksearch/upd',methods=["POST"])
 def getbookupdate():
     id = request.form.get("id")
@@ -84,4 +96,4 @@ def logs():
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0") #0.0.0.0 accessible from all IP
 
-#Added more filters in book search
+#Added basic page for user fine editing
