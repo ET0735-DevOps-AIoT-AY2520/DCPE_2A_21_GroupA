aa4b9da77fa145194d1cbfb9152c91cd5906aa48
Thiha <thhtut.24@ichat.sp.edu.sg>
2026-01-05 14:22:26 +0800

    Added comments to app.py

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -8,37 +8,45 @@ import db
 app=Flask(__name__)
 
 def getdbdata():
+    #call db.py to get books and profile
     db.getallbooks()
     db.getallprofile()
+    #take all the infomation from the global var in db.py
     location=db.locationdict[db.setlocation]
     books=[]
     profiles=[]
+    #convert books and profiles to dict format first
     for book in db.books:
         books.append(book.to_dict())
     for profile in db.profiles:
         profiles.append(profile.to_dict())
+        #send the result back
     return [books,profiles,location]
     
 
 
 @app.route('/')
 def index():
+    #get database data and send it to main menu
     [books,profiles,location]=getdbdata()
     return render_template("index.html",books=books,profiles=profiles,location=location)
 
 @app.route('/booksearch')
 def booksearch():
+    #get database data and send it to book search 
     [books,profiles,location]=getdbdata()
     return render_template("booksearch.html",books=books,profiles=profiles,location=location)
 
 @app.route('/usersearch')
 def usersearch():
+    #get database data and send it to user search
     [books,profiles,location]=getdbdata()
     return render_template("usersearch.html",books=books,profiles=profiles,location=location)
 
 #https://www.geeksforgeeks.org/html/retrieving-html-from-data-using-flask/
 @app.route('/booksearch/edit',methods=["POST"])
 def bookedit():
+    #get all the data from the body
     if request.method == "POST":
         id = request.form.get("id")
         title = request.form.get("title")
@@ -47,6 +55,7 @@ def bookedit():
         reserved = request.form.get("reserved")
         loaned = request.form.get("onloan")
         date= request.form.get("date")
+        #convert data into JSOn and send it to the next HTML for editing
         jsonbook={
             "id":id,
             "title":title,
@@ -74,6 +83,7 @@ def useredit():
 
 @app.route('/booksearch/upd',methods=["POST"])
 def getbookupdate():
+    #get all data from body of html
     id = request.form.get("id")
     title = request.form.get("title")
     location = request.form.get("location")
@@ -81,6 +91,7 @@ def getbookupdate():
     reserved = request.form.get("reserved")
     onloan = request.form.get("onloan")
     date= request.form.get("date")
+    #temporary confirmation of data downloading
     print(id)
     print(date)
     print(title)
@@ -88,14 +99,18 @@ def getbookupdate():
     print(loanadm)
     print(reserved)
     print(onloan)
+    #immediately send the user back to main screen after data acquired
     return redirect('/')
 
 @app.route('/usersearch/upd',methods=["POST"])
 def getuserupdate():
+    #download all data from body of html
     id = request.form.get("id")
     fine = request.form.get("fine")
+    #temporary confirmation of data downloading
     print(id)
     print(fine)
+    #send user back to main menu
     return redirect('/')
 
 @app.route('/logs')
@@ -106,4 +121,4 @@ def logs():
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0") #0.0.0.0 accessible from all IP
 
-#Added html to app.py for user edit
+#Added comments to app.py
