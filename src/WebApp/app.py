817f7be416d56fdb083b7c94b5f6e85e45bba780
Thiha <thhtut.24@ichat.sp.edu.sg>
2026-01-05 13:21:16 +0800

    added static and template folder with first page template

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 @app.route('/page2')
 def page2():
@@ -14,4 +14,4 @@ def page2():
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0") #0.0.0.0 accessible from all IP
 
-#added second route
+#added static and template folder with first page template
