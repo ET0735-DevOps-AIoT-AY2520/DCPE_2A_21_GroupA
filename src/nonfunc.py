5567c9a49438b734a3da2317d6e3ce0d1548282b
PorYu <YU07.24@ichat.sp.edu.sg>
2026-01-22 18:21:25 +0800

    Added mode switching add called db function

--- src/nonfunc.py
+++ src/nonfunc.py
@@ -1,24 +1,38 @@
 import threading as thread
 import time
 import db
-
+modecountdown=30
+mode=1
 countdown=1
 #Checking 
 def monitor():
     while True:
         #sleep half an hour and check mode
         time.sleep(0.1*10)
+        modeswitch()
         checkmode()
+
+def modeswitch():
+    global modecountdown
+    global mode
+    modecountdown=modecountdown-1
+    if modecountdown==0:
+        mode=0
+        print("mode switched")
+        
+
 
 def checkmode():
     global countdown
     if countdown!=0:
         countdown=countdown-1
-        print("Currently in inactive, do nothing")
     else:
         countdown=30
-        print("currently Active mode")
-        print("routine DB update")
+        if mode==1:
+            db.getallbooks()
+            db.getallprofile()
+            print("currently Active mode")
+            print("routine DB update")
 
 if __name__=="__main__":
     monitor()
