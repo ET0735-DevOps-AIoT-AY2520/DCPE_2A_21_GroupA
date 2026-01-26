66bc0b34d54d9341f7f6cbb2681b771cd59913df
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2026-01-26 14:19:10 +0800

    Changed date to fit requirements

--- src/db.py
+++ src/db.py
@@ -131,9 +131,9 @@ def calculatefine(input,timenow):
     for book in input:
         reserved_date = datetime.datetime.strptime(book["date"], "%Y-%m-%d").date()
         if book["extended"]==True:
-            cutoff=(timenow - datetime.timedelta(days=21))
+            cutoff=(timenow - datetime.timedelta(days=18))
         else:
-            cutoff=(timenow - datetime.timedelta(days=14))
+            cutoff=(timenow - datetime.timedelta(days=25))
         if reserved_date<cutoff:
             totaldayslate+=(cutoff-reserved_date).days
     totalfine=totaldayslate*0.15
